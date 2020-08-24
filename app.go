d67d892230c43615211d6b5c108c662610417ca2
Wendell <wendell@flipthedream.com>
2020-08-24 00:16:25 -0400

    Converted status box to list format in order to update indivudual lines.

--- app.go
+++ app.go
@@ -16,14 +16,40 @@ Health Endpoint:
 }
 */
 
+/*
+String to check balance and slash status
+curl -X POST -H "Content-Type: application/json" -d '{"pubkeys": ["0x90f70a6bbf31d38bb4e95a53ba87fc062b8858dcc45ec7c77174e891679f4e4edc2e6efb6f38aa11c7c66249c62cacdd"]}' http://localhost:5052/beacon/validators
+
+[
+  {
+    "pubkey": "0x90f70a6bbf31d38bb4e95a53ba87fc062b8858dcc45ec7c77174e891679f4e4edc2e6efb6f38aa11c7c66249c62cacdd",
+    "validator_index": 26595,
+    "balance": 31204700300,
+    "validator": {
+      "pubkey": "0x90f70a6bbf31d38bb4e95a53ba87fc062b8858dcc45ec7c77174e891679f4e4edc2e6efb6f38aa11c7c66249c62cacdd",
+      "withdrawal_credentials": "0x006fb0366db2f083f5618a1a41f7a520ea89909c66130e0952dee81bc752499f",
+      "effective_balance": 31000000000,
+      "slashed": false,
+      "activation_eligibility_epoch": 631,
+      "activation_epoch": 1665,
+      "exit_epoch": 18446744073709552000,
+      "withdrawable_epoch": 18446744073709552000
+    }
+  }
+]
+
+
+*/
+
 package main
 
 import (
 	"encoding/json"
-	//"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
 	"time"
 
@@ -38,6 +64,7 @@ type health struct {
 	SystemLoad  float64 `json:"sys_loadavg_1"`
 	Version     string
 	PeerCount   string
+	DBSize      string
 }
 
 /*
@@ -56,8 +83,9 @@ statusInfo := []string{
 var metrics = health{}
 var memGauge = widgets.NewGauge()
 var menuTest = widgets.NewParagraph()
-var textInfo = widgets.NewParagraph()
+var textInfo = widgets.NewList()
 var baseURL = "http://localhost:5052"
+var DBFile = "/var/lib/lighthouse/beacon-node/beacon/chain_db"
 
 //var healthURL = "http://localhost:5052/node/health"
 //var peerURL = "http://localhost:5052/network/peer_count"
@@ -74,13 +102,20 @@ func main() {
 	menuTest.BorderStyle.Fg = ui.ColorCyan
 
 	// Text information
-	textInfo.Title = "Status Bar"
-	textInfo.Text = "Loading..."
-	textInfo.SetRect(0, 5, 25, 10)
+
+	textInfo.Rows = []string{
+		"Node Status: ",
+		"System Load: ",
+		"Peer Count : ",
+		"DB Size   : ",
+	}
+
+	textInfo.SetRect(0, 5, 25, 5+len(textInfo.Rows)+2)
+	textInfo.TextStyle = ui.NewStyle(ui.ColorYellow)
 
 	// Gauge to show percent memory usage
 	memGauge.Title = "Mem Usage"
-	memGauge.SetRect(0, 10, 50, 13)
+	memGauge.SetRect(0, len(textInfo.Rows)+9, 50, len(textInfo.Rows)+12)
 	memGauge.Percent = 0
 	memGauge.BarColor = ui.ColorGreen
 	memGauge.BorderStyle.Fg = ui.ColorWhite
@@ -112,6 +147,7 @@ func main() {
 func showMemory() {
 	getHealth()
 	getPeers()
+	getDBSize()
 	if int(float64(metrics.MemoryUsed)/float64(metrics.MemoryTotal)*100) > 75 {
 		memGauge.BarColor = ui.ColorRed
 	} else {
@@ -119,7 +155,15 @@ func showMemory() {
 	}
 
 	memGauge.Percent = int(float64(metrics.MemoryUsed) / float64(metrics.MemoryTotal) * 100)
-	textInfo.Text = "System Load: " + FloatToString(metrics.SystemLoad) + "\n" + "Peer Count: " + metrics.PeerCount
+
+	textInfo.Rows = []string{
+		"Node Status: ",
+		"System Load: " + FloatToString(metrics.SystemLoad),
+		"Peer Count: " + metrics.PeerCount,
+		"DB Size   : " + metrics.DBSize,
+	}
+
+	// textInfo.Text = "System Load: " + FloatToString(metrics.SystemLoad) + "\n" + "Peer Count: " + metrics.PeerCount
 
 	ui.Render(menuTest)
 	ui.Render(textInfo)
@@ -188,4 +232,23 @@ func getVersion() {
 		log.Fatal(err)
 	}
 	metrics.Version = string(content)
+}
+
+func getDBSize() {
+
+	metrics.DBSize = strconv.FormatInt(DirSize(DBFile)/1024/1024/1024, 10) + " GB"
+}
+
+func DirSize(path string) int64 {
+	var size int64
+	filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
+	return size
 }
